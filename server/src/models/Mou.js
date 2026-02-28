4605749a4bb619e81b892f1e417a1781f97db994
m3ayush <ayushbhat0518@gmail.com>
2026-02-28 11:00:11 +0530

    updated expiry section

--- server/src/models/Mou.js
+++ server/src/models/Mou.js
@@ -65,4 +65,12 @@ const mouSchema = new mongoose.Schema(
 mouSchema.index({ status: 1, expiryDate: 1 });
 mouSchema.index({ faculty: 1, lastInteractionDate: 1 });
 
+// Auto-expire MoUs whose expiryDate has passed
+mouSchema.pre(['find', 'findOne'], async function () {
+  await mongoose.model('Mou').updateMany(
+    { status: 'active', expiryDate: { $lt: new Date() } },
+    { $set: { status: 'expired' } }
+  );
+});
+
 module.exports = mongoose.model('Mou', mouSchema);
